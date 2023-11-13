3492ad490e43e936562c93ae4538c2c8ecdde333
Ruslan Bahna <120433135+ruslanbahna@users.noreply.github.com>
2023-11-12 22:18:55 -0600

    comm

---
+++ .github/workflows/download_benchmark.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Download the benchmark JAR file
+curl -o renaissance-gpl-0.15.0.jar https://github.com/renaissance-benchmarks/renaissance/releases/download/v0.15.0/renaissance-gpl-0.15.0.jar
