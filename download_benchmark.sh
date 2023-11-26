1ac28731837609542734963c65e6302471263472
Ruslan Bahna <120433135+ruslanbahna@users.noreply.github.com>
2023-11-26 15:51:10 -0600

    Update download_benchmark.sh
    example

--- download_benchmark.sh
+++ download_benchmark.sh
@@ -2,3 +2,4 @@
 
 # Download the benchmark JAR file
 curl -o renaissance-gpl-0.15.0.jar https://github.com/renaissance-benchmarks/renaissance/releases/download/v0.15.0/renaissance-gpl-0.15.0.jar
+In summary, this program introduces some basic Java concepts, including variable declaration, loops, method definition, and function invocation, to create a more interactive version of the traditional "Hello, World!" program. It demonstrates the use of a simple loop, a method for adding numbers, and the printing of messages to the console.
