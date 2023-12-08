55c33ccf539efb1b079feaf6e585a07ad5ca47be
Ruslan Bahna <120433135+ruslanbahna@users.noreply.github.com>
2023-12-08 15:09:35 -0600

    Update download_benchmark.sh
    command

--- download_benchmark.sh
+++ download_benchmark.sh
@@ -4,3 +4,5 @@
 curl -o renaissance-gpl-0.15.0.jar https://github.com/renaissance-benchmarks/renaissance/releases/download/v0.15.0/renaissance-gpl-0.15.0.jar
 In summary, this program introduces some basic Java concepts, including variable declaration, loops, method definition, and function invocation, to create a more interactive version of the traditional "Hello, World!" program. It demonstrates the use of a simple loop, a method for adding numbers, and the printing of messages to the console.
 echo 'System.Drawing.Common|7.0.0' >> /usr/share/dotnet/packs/NETStandard.Library.Ref/2.1.0/data/PackageOverrides.txt
+# Add a command to remove the specific line from Microsoft.Build.Tasks.CodeAnalysis.deps.json
+RUN sed -i '/"System.Drawing.Common\/4.7.0"/d' /usr/share/dotnet/sdk/8.0.100/Roslyn/Microsoft.Build.Tasks.CodeAnalysis.deps.json
